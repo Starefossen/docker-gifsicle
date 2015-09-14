796531e787fadccdffa31a6a26547144166b5050
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-09-14 21:33:33 +0200

    Add gifsicle build container

---
+++ build/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export GIFSICLE_VERSION=$1
+export GIFSICLE_URL="https://github.com/kohler/gifsicle/archive/v${GIFSICLE_VERSION}.tar.gz"
+
+if [ -z ${GIFSICLE_VERSION} ]; then
+  echo "Usage: $0 [VERSION]";
+  exit 1
+fi
+
+curl -SLO ${GIFSICLE_URL} \
+  && tar -xvzf "v${GIFSICLE_VERSION}.tar.gz" \
+  && cd "gifsicle-${GIFSICLE_VERSION}" \
+  && autoreconf -i \
+  && ./configure --disable-gifview \
+  && make \
+  && tar -cvzf ../gifsicle.tar.gz -C src gifsicle gifdiff \
+  && cd .. \
+  && rm -rf "gifsicle-${GIFSICLE_VERSION}" "v${GIFSICLE_VERSION}.tar.gz"
